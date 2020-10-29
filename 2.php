e28b5ea8cfdbbbe194b3061881909681c50a04fc
DwiqiAfif <71859496+DwiqiAfif@users.noreply.github.com>
2020-10-29 20:04:15 +0700

    Add files via upload

---
+++ 2.php
@@ -0,0 +1,8 @@
+<?php
+	for ($i=1; $i <= 10; $i++) { 
+		echo "<br/> Hallo... Nama saya Muhammad Dwiqi Afifuddin";
+		echo "<br/> Saya Mahasiswa UNIGA, Prodi Sistem Informasi";
+		echo "<br/> Asal Mojowarno Jombang Jawa Timur";
+		echo "<br/>";
+	}
+?>
